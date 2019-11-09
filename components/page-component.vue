16fa8467b8ad75b5c5dd716123fe48316c257272
MrZordex <zordex@gmail.com>
2019-11-09 21:25:22 +0200

    does not work after npm generate

---
+++ components/page-component.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <carousel>
+      <slide>slide 1</slide>
+      <slide>slide 2</slide>
+      <slide>slide 3</slide>
+      <slide>slide 4</slide>
+    </carousel>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="container">
+    <page-component />
+  </div>
+</template>
+
+<script>
+import PageComponent from "~/components/page-component";
+
+export default {
+  components: {
+    PageComponent
+  }
+};
+</script>
